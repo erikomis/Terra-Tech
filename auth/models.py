0a79724501be1dbedadcacd5ce1f507ef8e8e461
erik-martins <erik.ozzetti.martins@gmail.com>
2024-03-02 18:45:03 -0300

    feat: model user

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser,BaseUserManager
 
-# Create your models here.
+class UserManager(BaseUserManager) :
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.is_active = True
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.CharField(max_length=128, null=True)
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
