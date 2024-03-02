b6cdd51a99046c3e0dda8daa4b0e25d3d5515c92
erik-martins <erik.ozzetti.martins@gmail.com>
2024-03-02 20:42:54 -0300

    feat:  dotenv config

--- manage.py
+++ manage.py
@@ -2,13 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from dotenv import load_dotenv
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
+        load_dotenv()
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
