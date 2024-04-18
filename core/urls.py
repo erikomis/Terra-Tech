ba0e10594c39d2fed09157434ce3c90a28b39ff0
erik-martins <erik.ozzetti.martins@gmail.com>
2024-04-17 23:29:52 -0300

    feat: urls

--- core/urls.py
+++ core/urls.py
@@ -1,27 +1,20 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path , include
+from django.conf import settings
+from django.conf.urls.static import static
 from auth_api import views
-from knox.views import LogoutView, LogoutAllView
 from photos import views as photo_views
+from knox.views import LogoutView, LogoutAllView
+
+
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('activate/<str:token>/', views.ActivateAccount.as_view(), name='activate-account'),
+    path('forgot-password/', views.forgot_password.as_view()),
+    path('verify-token/', views.VerifyToken.as_view()),  
+    path('reset-password/', views.ResetPassword.as_view(), name='reset-password'),
     path('create-user/', views.CreateUserAPI.as_view()),
     path('update-user/<int:pk>/', views.UpdateUserAPI.as_view()),
     path('login/', views.LoginAPIView.as_view()),
@@ -30,5 +23,4 @@ urlpatterns = [
     path('photos',  photo_views.UPLOAD_PHOTO.as_view()),
     path('photos/<int:pk>',  photo_views.Delete_Photo.as_view()),
     path('photos/', photo_views.List_Photos.as_view()),
-
 ]
